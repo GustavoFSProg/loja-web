e8dc994fc725a4be584e88f1d1e97ae224f71abc
Gustavo <gustavosohne38@gmail.com>
2020-12-21 09:33:59 -0300

    wwwww

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Listagem from './pages/Listagem'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 
